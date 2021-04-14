b95e9a2459bb0b88584fad0f18d13c8b5a2181ee
yakushijin <yakushijin@yakusys.com>
2021-04-14 14:02:52 +0900

    mysqlとredis追加（暫定）

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const { Client } = require("pg");
+const mysql = require("mysql");
+const redis = require("redis");
 const Log4js = require("log4js");
 Log4js.configure("log-config.json");
 
@@ -41,6 +43,37 @@ app.get("/postgres", (req, res) => {
   CC.connect()
     .then(() => res.send("dbok"))
     .catch((e) => console.log(e));
+});
+
+app.get("/mysql", (req, res) => {
+  const dbName = "mysql";
+  const dbUser = "mysqlUser";
+  const dbPassword = "mysqlPass1234!";
+  const dbHost = "mysqlIPorDomain";
+  const dbQuery = "SELECT * FROM hoge";
+
+  const connection = mysql.createConnection({
+    host: dbHost,
+    user: dbUser,
+    password: dbPassword,
+    database: dbName,
+  });
+  connection.connect((err) => {
+    if (err) {
+      res.send("ng");
+      return;
+    }
+    res.send("ok");
+  });
+});
+
+app.get("/redis", (req, res) => {
+  const Host = "redisIPorDomain";
+
+  const client = redis.createClient(6379, Host);
+  client.on("monitor", function (time, args, rawReply) {
+    res.send("ok");
+  });
 });
 
 app.listen(port, () => {});
